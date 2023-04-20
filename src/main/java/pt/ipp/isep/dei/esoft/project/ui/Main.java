a85befdeec7df858f78e8ffa15dabfafc97233cb
Tiago Correia <106349052+tiagohcorreia@users.noreply.github.com>
2023-04-20 17:30:59 +0100

    Code fixes

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/Main.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/Main.java
@@ -1,12 +1,12 @@
-package pt.ipp.isep.dei.esoft.project.ui;
+/*package pt.ipp.isep.dei.esoft.project.ui;
 
 import pt.ipp.isep.dei.esoft.project.ui.console.menu.MainMenuUI;
 
 public class Main {
 
     public static void main(String[] args) {
-        /*Bootstrap bootstrap = new Bootstrap();
-        bootstrap.run();*/
+        *//*Bootstrap bootstrap = new Bootstrap();
+        bootstrap.run();*//*
 
         try {
             MainMenuUI menu = new MainMenuUI();
@@ -15,4 +15,4 @@ public class Main {
             e.printStackTrace();
         }
     }
-}
+}*/
--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/AdminUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/AdminUI.java
@@ -3,7 +3,7 @@ package pt.ipp.isep.dei.esoft.project.ui.console.menu;
 
 import pt.ipp.isep.dei.esoft.project.ui.console.CreateTaskUI;
 //import pt.ipp.isep.dei.esoft.project.ui.console.ShowTextUI;
-import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
+//import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +26,7 @@ public class AdminUI implements Runnable {
 
         int option = 0;
         do {
-            option = Utils.showAndSelectIndex(options, "\n\nAdmin Menu:");
+            //option = Utils.showAndSelectIndex(options, "\n\nAdmin Menu:");
 
             if ((option >= 0) && (option < options.size())) {
                 options.get(option).run();
