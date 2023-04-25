19e926ae23df013eda6bc0377a492510da7e4309
Tiago Correia <106349052+tiagohcorreia@users.noreply.github.com>
2023-04-25 15:59:18 +0100

    Code: RegisterEmployeeUI and RegisterEmployeeController classes updated

--- src/main/java/app/controller/RegisterEmployeeController.java
+++ src/main/java/app/controller/RegisterEmployeeController.java
@@ -1,19 +1,34 @@
 package app.controller;
 
 import app.domain.model.Agency;
+import app.domain.model.Role;
 import app.domain.repository.RegisterEmployeeRepository;
-
-
-import javax.management.relation.Role;
 
 public class RegisterEmployeeController {
 
     private RegisterEmployeeRepository employeeRepository;
+
+    public RegisterEmployeeController(RegisterEmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public String getRole() {
+        return Role.asString();
+    }
+
+    public String getAgency() {
+        return Agency.asString();
+    }
+
+
 
     public void createEmployee(String employeeName, int passportNumber, int taxNumber, String address, String eMail,
                                int telephoneNumber, Role role, Agency agency) {
 
 
     }
+
+
+
 
 }
