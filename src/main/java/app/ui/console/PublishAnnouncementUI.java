369d8bf96a12820123143a4c98404b11ee03d5ea
NilsaGil_1221792 <1221792@isep.ipp.pt>
2023-04-28 00:41:11 +0100

    US2_Implementation

--- src/main/java/app/ui/console/PublishAnnouncementUI.java
+++ src/main/java/app/ui/console/PublishAnnouncementUI.java
@@ -1,4 +1,42 @@
 package app.ui.console;
 
+import app.controller.PublishAnnouncementController;
+
+import java.util.Scanner;
+
 public class PublishAnnouncementUI {
+
+    private Scanner scanner;
+    private PublishAnnouncementController controller;
+
+    public PublishAnnouncementUI(){
+        scanner = new Scanner(System.in);
+        controller = new PublishAnnouncementController();
+    }
+
+    public void run() {
+        boolean exit = false;
+        do {
+            System.out.println("Hello, Agent");
+            System.out.println("1. Publish Announcement");
+            System.out.println("2. Exit");
+            System.out.println("Select an option:");
+            String option = scanner.nextLine();
+
+            switch (option){
+                case "1":
+                    publishAnnouncement();
+                    break;
+                case "2":
+                    exit = true;
+                default:
+                    System.out.println("Invalid option!");
+                    break;
+
+            }
+        } while (!exit);
+        scanner.close();
+    }
+
+    private void publishAnnouncement()
 }
