cfaad63e30c5de70a2e442866071658ae644bc96
Vitor Fardilha <1211273@isep.ipp.pt>
2023-04-27 16:59:12 +0100

    US04- RegisterPropertyController updated the repository name

--- src/main/java/app/controller/RegisterPropertyController.java
+++ src/main/java/app/controller/RegisterPropertyController.java
@@ -1,6 +1,7 @@
 package app.controller;
 
 import app.domain.model.Employee;
+import app.domain.repository.AnnouncementRepository;
 import app.domain.repository.PropertyRepository;
 import app.domain.repository.RegisterEmployeeRepository;
 import app.domain.repository.Repositories;
@@ -10,15 +11,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RegisterPropertyController {
-    private PropertyRepository propertyRepository;
+    private AnnouncementRepository announcementRepository;
     Repositories repositories = Repositories.getInstance();
     RegisterEmployeeRepository employeeRepository = repositories.getEmployeeRepository();
-    public RegisterPropertyController(PropertyRepository propertyRepository) {
-        this.propertyRepository = propertyRepository;
+    public RegisterPropertyController(AnnouncementRepository announcementRepository) {
+        this.announcementRepository = announcementRepository;
     }
     public void createProperty(String sellOrRent, String typeProperty, int bedrooms, int bathrooms, int parking,
                                ArrayList<String> equipmentList, String basement, String inhabitalLoft, String sunExposure,
                                int area, String location, int centreDistance, double price, ArrayList<String> photographs, Employee agent) {
+
 
     }
     public List<Employee> getAgent() {
