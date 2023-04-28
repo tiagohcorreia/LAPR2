92f43d77ddcad23c4b186b4caac473390f845727
Vitor Fardilha <1211273@isep.ipp.pt>
2023-04-28 11:27:24 +0100

    US04 - Update RegisterPropertyController using enum lists and validate city

--- src/main/java/app/controller/RegisterPropertyController.java
+++ src/main/java/app/controller/RegisterPropertyController.java
@@ -1,36 +1,41 @@
 package app.controller;
 
 import app.domain.model.*;
-import app.domain.repository.AnnouncementRepository;
-import app.domain.repository.RegisterEmployeeRepository;
-import app.domain.repository.Repositories;
+import app.domain.repository.*;
+import app.domain.shared.SunExposure;
+import app.domain.shared.TypeOfBusiness;
+import app.domain.shared.TypeOfProperty;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RegisterPropertyController {
     private AnnouncementRepository announcementRepository;
     Repositories repositories = Repositories.getInstance();
     RegisterEmployeeRepository employeeRepository = repositories.getEmployeeRepository();
+    StateRepository stateRepository=repositories.getStateRepository();
+    DistrictRepository districtRepository=repositories.getDistrictRepository();
+    CityRepository cityRepository=repositories.getCityRepository();
     public RegisterPropertyController(AnnouncementRepository announcementRepository) {
         this.announcementRepository = announcementRepository;
     }
-    public void createAnnouncement(String sellOrRent, String typeProperty, int numberOfBedrooms, int numberOfBathrooms, int numberOfParkingSpaces,
-                               ArrayList<String> equipmentList,boolean hasBasement, boolean hasInhabitalLoft, boolean sunExposure,
-                               int area, City location, int cityCentreDistance, double price, ArrayList<String> photographs, Employee agent) {
+    public void createAnnouncement(TypeOfBusiness sellOrRent, int posTypeOfProperty, int numberOfBedrooms, int numberOfBathrooms, int numberOfParkingSpaces,
+                               ArrayList<String> equipmentList,boolean hasBasement, boolean hasInhabitalLoft, SunExposure sunExposure,
+                               int area, City location, int cityCentreDistance, float price, ArrayList<String> photographs, Employee agent) {
 
-        if (typeProperty.equals("A")){
+        if (posTypeOfProperty==2){
             Property property= new Apartment(area,location,cityCentreDistance,photographs,numberOfBedrooms,numberOfBathrooms,numberOfParkingSpaces,equipmentList);
-            Announcement announcement= new Announcement(false,price,null ,sellOrRent,property,agent);
+            Announcement announcement= new Announcement(false,price,0 ,sellOrRent,property,agent);
             this.announcementRepository.createAnnouncement(announcement);
-        } else if (typeProperty.equals("H")) {
+        } else if (posTypeOfProperty==1) {
             Property property= new House(area,location,cityCentreDistance,photographs,numberOfBedrooms,numberOfBathrooms,numberOfParkingSpaces,equipmentList,hasBasement,hasInhabitalLoft,sunExposure);
-            Announcement announcement= new Announcement(false,price,null ,sellOrRent,property,agent);
+            Announcement announcement= new Announcement(false,price,0 ,sellOrRent,property,agent);
             this.announcementRepository.createAnnouncement(announcement);
         }else {
             Property property= new Land(area,location,cityCentreDistance,photographs);
-            Announcement announcement= new Announcement(false,price,null ,sellOrRent,property,agent);
+            Announcement announcement= new Announcement(false,price,0 ,sellOrRent,property,agent);
             this.announcementRepository.createAnnouncement(announcement);
         }
 
@@ -48,5 +53,21 @@ public class RegisterPropertyController {
     }
     public Employee getEmployee(String name){
         return employeeRepository.getEmployee(name);
+    }
+
+    public City getCity(String city){
+        return cityRepository.findByName(city);
+    }
+
+    public List<SunExposure> getSunExposureAsList() {
+        return Arrays.stream(SunExposure.values()).toList();
+    }
+
+    public List<TypeOfProperty> getTypeOfPropertyAsList() {
+        return Arrays.stream(TypeOfProperty.values()).toList();
+    }
+
+    public List<TypeOfBusiness> getTypeOfBusinessAsList() {
+        return Arrays.stream(TypeOfBusiness.values()).toList();
     }
 }
