0c0ea10577102f28e0c4dfec20adb0f1e8343ed3
1210789 <1210789@isep.ipp.pt>
2023-04-29 19:09:16 +0100

    Refactored DisplayAnnouncementsUI into multiple functions to improve readability and updated the SD accordingly

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/DisplayAnnouncementsUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/DisplayAnnouncementsUI.java
@@ -10,21 +10,21 @@ import java.util.Collections;
 import java.util.List;
 
 public class DisplayAnnouncementsUI implements Runnable{
+    private static final DisplayAnnouncementsController controller = new DisplayAnnouncementsController();
 
     @Override
     public void run() {
-        displayAnnouncements();
+        runUS();
     }
 
-    private boolean displayAnnouncements(){
-        DisplayAnnouncementsController controller = new DisplayAnnouncementsController();
-        List<List<Object>> availableFields = controller.getAvailableFields();
+    public List<List<Object>> getAvailableFields(){
+        return controller.getAvailableFields();
+    }
 
-        if (availableFields.get(0) == null){
+    public void displayAvailableFields(List<List<Object>> availableFields){
+        if (availableFields.get(0).size() == 0){
             System.out.println("There are no announcements in the system.");
-            return true;
         }
-
         else {
             System.out.println("SEARCH FILTER");
             System.out.println("- Select one from the following options");
@@ -47,104 +47,149 @@ public class DisplayAnnouncementsUI implements Runnable{
                 System.out.print(fields.toString() + " ");
             }
             System.out.println();
+        }
+    }
 
-            //Getting user input
-            String selectedTypeOfBusiness=null;
+    public String[] requestFilterData(List<List<Object>> availableFields){
+        String[] selectedData = new String[3];
+
+        //Getting user data
+        //  -Type of business
+        String selectedTypeOfBusiness=null;
+        do {
+            try {
+                selectedTypeOfBusiness = Utils.readLineFromConsole("Type of business: ").trim().toUpperCase();
+                selectedData[0] = selectedTypeOfBusiness;
+            } catch (Exception e){
+                System.out.println("Please try again.");
+            }
+        } while (!availableFields.get(0).toString().contains(selectedTypeOfBusiness) && !selectedTypeOfBusiness.equals(""));
+
+        //  -Type of property
+        String selectedTypeOfProperty=null;
+        do {
+            try {
+                selectedTypeOfProperty = Utils.readLineFromConsole("Type of property: ").trim().toUpperCase();
+                selectedData[1] = selectedTypeOfProperty;
+            } catch (Exception e){
+                System.out.println("Please try again.");
+            }
+        } while (!availableFields.get(1).toString().contains(selectedTypeOfProperty) && !selectedTypeOfProperty.equals(""));
+
+//        int selectedNumberOfBedrooms = -1;
+//        String input = null;
+//        if(!selectedTypeOfProperty.equals("LAND")) {
+//            do {
+//                try {
+//                    input = Utils.readLineFromConsole("Number of bedrooms: ").trim();
+//                    selectedNumberOfBedrooms = (input.equals("")) ? -1 : Integer.parseInt(input);
+//                    selectedData[2] = String.valueOf(selectedNumberOfBedrooms);
+//                } catch (Exception e){
+//                    selectedNumberOfBedrooms = -1;
+//                    System.out.println("Please try again.");
+//                }
+//            } while (!availableFields.get(2).contains(selectedNumberOfBedrooms) && selectedNumberOfBedrooms == -1 && !input.equals(""));
+//        }
+
+        //  -Number of bedrooms
+        //(only displayed if the selected property type is not "LAND")
+        String selectedNumberOfBedrooms = null;
+        if(!selectedTypeOfProperty.equals("LAND")) {
             do {
                 try {
-                    selectedTypeOfBusiness = Utils.readLineFromConsole("Type of business: ").trim().toUpperCase();
+                    selectedNumberOfBedrooms = Utils.readLineFromConsole("Number of bedrooms: ").trim();
+                    selectedData[2] = selectedNumberOfBedrooms;
                 } catch (Exception e){
                     System.out.println("Please try again.");
                 }
-            } while (!availableFields.get(0).toString().contains(selectedTypeOfBusiness) && !selectedTypeOfBusiness.equals(""));
+//            } while (!availableFields.get(2).contains(selectedNumberOfBedrooms) && selectedNumberOfBedrooms == -1 && !input.equals(""));
+            } while (!availableFields.get(1).toString().contains(String.valueOf(selectedNumberOfBedrooms)) && !selectedNumberOfBedrooms.equals(""));
 
-            String selectedTypeOfProperty=null;
-            do {
-                try {
-                    selectedTypeOfProperty = Utils.readLineFromConsole("Type of property: ").trim().toUpperCase();
-                } catch (Exception e){
-                    System.out.println("Please try again.");
-                }
-            } while (!availableFields.get(1).toString().contains(selectedTypeOfProperty) && !selectedTypeOfProperty.equals(""));
+    }
 
-            int selectedNumberOfBedrooms = -1;
-            String input = null;
-            if(!selectedTypeOfProperty.equals("LAND")) {
-                do {
-                    try {
-                        input = Utils.readLineFromConsole("Number of bedrooms: ").trim();
-                        selectedNumberOfBedrooms = (input.equals("")) ? -1 : Integer.parseInt(input);
-                    } catch (Exception e){
-                        selectedNumberOfBedrooms = -1;
-                        System.out.println("Please try again.");
-                    }
-                } while (!availableFields.get(2).contains(selectedNumberOfBedrooms) && selectedNumberOfBedrooms == -1 && !input.equals(""));
-            }
+        if (selectedData[0].equals("") && selectedData[1].equals("") && selectedData[2].equals(""))
+            return null;
+        else
+            return selectedData;
+    }
 
-            //Get matching announcements
-            //Default order is oldest first
-            List<Announcement> announcements;
-            //TO-FIX
-            if (selectedTypeOfBusiness.equals("") && selectedTypeOfProperty.equals("") && selectedNumberOfBedrooms == -1){
-                announcements = controller.getAllVisibleAnnouncements();
-            } else {
-                announcements = controller.getAnnouncements(selectedTypeOfBusiness, selectedTypeOfProperty, selectedNumberOfBedrooms);
-            }
+    public List<Announcement> getMatchingAnnouncements(String[] selectedData){
+        //List<Announcement> matchingAnnouncements = controller.getAnnouncements(selectedData);
+        return null;
+    }
 
-            Collections.reverse(announcements);
+    private boolean runUS(){
+        List<List<Object>> availableFields = getAvailableFields();
+        displayAvailableFields(availableFields);
+        String[] selectedData = requestFilterData(availableFields);
 
-            for (Announcement announcement : announcements) {
-                System.out.println(announcement);
-            }
-//            while (sortingMode != 0) {
+
+
 //
-//                do {
-//                    sortingMode = Utils.readIntegerFromConsole("Sorting modes:\n1 - Price\n2 - Parish\n0 - Exit\nSort by: ");
-//                    if (sortingMode != 1 && sortingMode != 2 && sortingMode != 0) {
+//            //Get matching announcements
+//            //Default order is oldest first
+//            List<Announcement> announcements;
+//            //TO-FIX
+//            if (selectedTypeOfBusiness.equals("") && selectedTypeOfProperty.equals("") && selectedNumberOfBedrooms == -1){
+//                announcements = controller.getAllVisibleAnnouncements();
+//            } else {
+//                announcements = controller.getAnnouncements(selectedTypeOfBusiness, selectedTypeOfProperty, selectedNumberOfBedrooms);
+//            }
+//
+//            Collections.reverse(announcements);
+//
+//            for (Announcement announcement : announcements) {
+//                System.out.println(announcement);
+//            }
+////            while (sortingMode != 0) {
+////
+////                do {
+////                    sortingMode = Utils.readIntegerFromConsole("Sorting modes:\n1 - Price\n2 - Parish\n0 - Exit\nSort by: ");
+////                    if (sortingMode != 1 && sortingMode != 2 && sortingMode != 0) {
+////                        System.out.println("Invalid selection, please try again");
+////                    }
+////                } while (sortingMode != 1 && sortingMode != 2);
+////
+////                if (sortingMode == 1) {
+////                    ListingPriceComparator listingPriceComparator = new ListingPriceComparator();
+////                    matchingListings.sort(listingPriceComparator);
+////                    for (Announcement matchingListing : matchingListings) {
+////                        System.out.println(matchingListing);
+////                    }
+////                } else {
+////
+////                }
+////            }
+//
+//            int sortingMode = -1;
+//            boolean sortingModeIsValid = false;
+//            while (sortingMode != 0){
+//                do{
+//                    sortingMode = Utils.readIntegerFromConsole("Sorting modes:\n1 - Price\n2 - City\n0 - Exit\nSort by: ");
+//                    if (sortingMode == 1 || sortingMode == 2 || sortingMode == 0) {
+//                        sortingModeIsValid = true;
+//                    }
+//                    else {
 //                        System.out.println("Invalid selection, please try again");
 //                    }
-//                } while (sortingMode != 1 && sortingMode != 2);
+//                } while (!sortingModeIsValid);
 //
 //                if (sortingMode == 1) {
-//                    ListingPriceComparator listingPriceComparator = new ListingPriceComparator();
-//                    matchingListings.sort(listingPriceComparator);
-//                    for (Announcement matchingListing : matchingListings) {
-//                        System.out.println(matchingListing);
+//                    AnnouncementPriceComparator announcementPriceComparator = new AnnouncementPriceComparator();
+//                    announcements.sort(announcementPriceComparator);
+//                    for (Announcement announcement : announcements) {
+//                        System.out.println(announcement);
 //                    }
-//                } else {
-//
+//                } else if (sortingMode == 2){
+//                    AnnouncementLocationComparator announcementLocationComparator = new AnnouncementLocationComparator();
+//                    announcements.sort(announcementLocationComparator);
+//                    for (Announcement announcement : announcements) {
+//                        System.out.println(announcement);
+//                    }
 //                }
 //            }
-
-            int sortingMode = -1;
-            boolean sortingModeIsValid = false;
-            while (sortingMode != 0){
-                do{
-                    sortingMode = Utils.readIntegerFromConsole("Sorting modes:\n1 - Price\n2 - City\n0 - Exit\nSort by: ");
-                    if (sortingMode == 1 || sortingMode == 2 || sortingMode == 0) {
-                        sortingModeIsValid = true;
-                    }
-                    else {
-                        System.out.println("Invalid selection, please try again");
-                    }
-                } while (!sortingModeIsValid);
-
-                if (sortingMode == 1) {
-                    AnnouncementPriceComparator announcementPriceComparator = new AnnouncementPriceComparator();
-                    announcements.sort(announcementPriceComparator);
-                    for (Announcement announcement : announcements) {
-                        System.out.println(announcement);
-                    }
-                } else if (sortingMode == 2){
-                    AnnouncementLocationComparator announcementLocationComparator = new AnnouncementLocationComparator();
-                    announcements.sort(announcementLocationComparator);
-                    for (Announcement announcement : announcements) {
-                        System.out.println(announcement);
-                    }
-                }
-            }
-        }
-
+//
+//
         return false;
     }
 }
