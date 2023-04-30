1036bbb51bebbcd82dd29a028df1906f615c4c31
Vitor Fardilha <1211273@isep.ipp.pt>
2023-04-30 02:05:27 +0100

    US04 - AuthenticationUI update

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/authorization/AuthenticationUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/authorization/AuthenticationUI.java
@@ -5,6 +5,7 @@ import pt.ipp.isep.dei.esoft.project.application.controller.authorization.Authen
 import pt.ipp.isep.dei.esoft.project.ui.console.menu.AdminUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.menu.AgentUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.menu.MenuItem;
+import pt.ipp.isep.dei.esoft.project.ui.console.menu.OwnerUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
 import pt.isep.lei.esoft.auth.mappers.dto.UserRoleDTO;
 
@@ -47,6 +48,7 @@ public class AuthenticationUI implements Runnable {
     private List<MenuItem> getMenuItemForRoles() {
         List<MenuItem> rolesUI = new ArrayList<>();
         rolesUI.add(new MenuItem(AuthenticationController.ROLE_ADMIN, new AdminUI()));
+        rolesUI.add(new MenuItem(AuthenticationController.ROLE_OWNER, new OwnerUI()));
         rolesUI.add(new MenuItem(AuthenticationController.ROLE_EMPLOYEE, new AgentUI()));
 
         //TODO: Complete with other user roles and related RoleUI
