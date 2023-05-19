157e39a00ac502f36cd5844fa5b819a88d54b9d3
1210789 <1210789@isep.ipp.pt>
2023-05-19 23:52:30 +0100

    US12 - Added FileHandler tests and exception type InvalidFileTypeException

--- src/main/java/pt/ipp/isep/dei/esoft/project/utils/FileHandler.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/utils/FileHandler.java
@@ -1,37 +1,89 @@
 package pt.ipp.isep.dei.esoft.project.utils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import pt.ipp.isep.dei.esoft.project.exceptions.InvalidFileTypeException;
+
+import javax.management.InvalidAttributeValueException;
+import java.io.*;
+import java.util.*;
 
 public class FileHandler {
-
-    private static final String FIELD_DELIMITER = ",";
-    private static final String LINE_DELIMITER = "\n";
     private static final String DELIMITER = ",";
 
-    //
-    public static File readFile(String filePath) throws FileNotFoundException{
-        return new File(filePath);
+    public static File readFile(String filePath) throws FileNotFoundException {
+        File file = new File(filePath);
+
+        if (!file.exists())
+            throw new FileNotFoundException("File not found.");
+        else if(!file.isFile() || !file.canRead())
+            throw new IllegalArgumentException("The selected file is invalid.");
+        else
+            return file;
     }
 
-    public static List<?> readCSV(File file) throws FileNotFoundException {
+    public static List<?> readCSV(File file) throws InvalidAttributeValueException, InvalidFileTypeException {
+        //Check if the filename ends with ".csv"
+        String[] filepath = file.getAbsolutePath().split("\\.");
+        String extension = filepath[filepath.length-1];
+        if(!extension.equalsIgnoreCase("csv"))
+            throw new InvalidFileTypeException("Selected file is not a CSV file");
+
         List<List<String>> csv = new ArrayList<>();
-        Scanner sc = new Scanner(file);
-        sc.useDelimiter(DELIMITER);
-        while (sc.hasNextLine()){
-            List<String> thisLine = new ArrayList<>();
-            String line = sc.nextLine();
-            String[] lineElements = line.split(",");
-            for(String element: lineElements){
-                thisLine.add(element);
+        try {
+            Scanner sc = new Scanner(file);
+            sc.useDelimiter(DELIMITER);
+            while (sc.hasNextLine()) {
+                List<String> thisLine = new ArrayList<>();
+                String line = sc.nextLine();
+                String[] lineElements = line.split(",");
+                Collections.addAll(thisLine, lineElements);
+                csv.add(thisLine);
             }
-            csv.add(thisLine);
+        } finally {
+            return csv;
         }
-        sc.close();
-        return csv;
+    }
+
+    public static boolean appendToFile(String filepath, String content){
+        File file = new File(filepath);
+        try {
+            Writer writer = new FileWriter(file, true);
+            try{
+                writer.write(content);
+            } catch (Exception e){
+                writer.close();
+                return false;
+            } finally {
+                writer.close();
+            }
+        } catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean createFile(String filepath, String content) {
+        File file = new File(filepath);
+        try {
+            Writer writer = new FileWriter(file);
+            try {
+                writer.write(content);
+            } finally {
+                writer.close();
+            }
+        } catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean deleteFile(String filepath){
+        boolean result;
+        try{
+            File file = readFile(filepath);
+            result = file.delete();
+        } catch (FileNotFoundException e){
+            return false;
+        }
+        return result;
     }
 }
--- src/test/java/pt/ipp/isep/dei/esoft/project/utils/FileHandlerTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/utils/FileHandlerTest.java
@@ -1,12 +1,17 @@
 package pt.ipp.isep.dei.esoft.project.utils;
 
-import pt.ipp.isep.dei.esoft.project.utils.FileHandler;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import pt.ipp.isep.dei.esoft.project.exceptions.InvalidFileTypeException;
 
 import org.junit.jupiter.api.Test;
 
+import javax.management.InvalidAttributeValueException;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,19 +19,77 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FileHandlerTest {
 
-    private static final String FILEPATH = "FileHandlerTestFile.csv";
+    private static final String CSV_FILE_FILEPATH = "example_csv_file.csv";
+    private static final String TEST_FILE_FILEPATH = "example_file.txt";
+    private static final String TEST_FILE_2_FILEPATH = "example_file_2.txt";
+    private static final String EMPTY_FILE_FILEPATH = "empty_file.txt";
+    private static final List<List<String>> data = new ArrayList<>();
+
+    private static final String EXAMPLE_LINE_1 = "This is a, test file!";
+    private static final String EXAMPLE_LINE_2 = "And something else...";
 
 
-    File generateTestFile() throws FileNotFoundException {
+    //private final File csvFile = generateTestFile();
+
+//    public FileHandlerTest(){
+//        //final File csvFile = generateTestFile();
+//
+//        //Generate files
+//        FileHandler.createFile(CSV_FILE_FILEPATH, generateCSVString());
+//        FileHandler.createFile(TEST_FILE_FILEPATH,"This is a, test file!");
+//        FileHandler.createFile(EMPTY_FILE_FILEPATH,"");
+//
+//        //Generate CSV data as list of string lists
+//        List<List<String>> data = new ArrayList<>();
+//        for (int i = 0; i < 10; i++) {
+//            List<String> line = new ArrayList<>();
+//            for (int j = 0; j < 10; j++) {
+//                line.add("L"+(i+1)+"E"+(j+1));
+//            }
+//            data.add(line);
+//        }
+//    }
+
+    @BeforeAll
+    static void arrange(){
+        //Generate files
+        FileHandler.createFile(CSV_FILE_FILEPATH, generateCSVString());
+        FileHandler.createFile(TEST_FILE_FILEPATH,EXAMPLE_LINE_1);
+        FileHandler.createFile(TEST_FILE_2_FILEPATH,EXAMPLE_LINE_1 + "\n" + EXAMPLE_LINE_2);
+        FileHandler.createFile(EMPTY_FILE_FILEPATH,"");
+
+        //Generate CSV data as list of string lists
+        for (int i = 0; i < 10; i++) {
+            List<String> line = new ArrayList<>();
+            for (int j = 0; j < 10; j++) {
+                line.add("L"+(i+1)+"E"+(j+1));
+            }
+            data.add(line);
+        }
+    }
+
+
+
+    @AfterAll
+    static void cleanup(){
+            FileHandler.deleteFile(CSV_FILE_FILEPATH);
+            FileHandler.deleteFile(TEST_FILE_FILEPATH);
+            FileHandler.deleteFile(TEST_FILE_2_FILEPATH);
+            FileHandler.deleteFile(EMPTY_FILE_FILEPATH);
+    }
+
+
+
+    private File generateTestFile() throws FileNotFoundException {
         String testString = generateCSVString();
-        File file = new File(FILEPATH);
+        File file = new File(CSV_FILE_FILEPATH);
         PrintWriter writer = new PrintWriter(file);
         writer.write(testString);
         writer.close();
         return file;
     }
 
-    String generateCSVString(){
+    private static String generateCSVString(){
         String testString = new String();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 10; i++) {
@@ -39,7 +102,7 @@ class FileHandlerTest {
         return testString;
     }
 
-    List<List<String>> CSVStringToList(String csv){
+    private List<List<String>> CSVStringToList(String csv){
         List<List<String>> csvAsList = new ArrayList<>();
         String[] lines = csv.split("\n");
         for (String line: lines) {
@@ -54,27 +117,93 @@ class FileHandlerTest {
     @Test
     void ensureReadFileWorks() throws FileNotFoundException {
         //Arrange
-        File file = generateTestFile();
+        //File file = generateTestFile();
 
         //Act
-        File resultFile = FileHandler.readFile(FILEPATH);
+        File resultFile = FileHandler.readFile(CSV_FILE_FILEPATH);
 
         //Assert
-        assertEquals(file, resultFile);
+        assertEquals(new File(CSV_FILE_FILEPATH), resultFile);
+        //assertEquals(file, resultFile);
+    }
+
+
+    @Test
+    void ensureReadCSVThrowsInvalidFileTypeException() throws FileNotFoundException {
+        //Arrange
+        FileHandler.createFile(TEST_FILE_FILEPATH,"This is a, test file!");
+        File file = FileHandler.readFile(TEST_FILE_FILEPATH);
+
+        //Act & Assert
+        assertThrows(InvalidFileTypeException.class, () -> {
+            List<?> csv = FileHandler.readCSV(file);
+        });
     }
 
     @Test
-    void ensureReadCSVWorks() throws FileNotFoundException {
+    void ensureReadCSVThrowsFileIsEmptyException(){
         //Arrange
-        File testfile = generateTestFile();
-        String testString = generateCSVString();
-        List<?> testCSVList = CSVStringToList(testString);
+
+
+    }
+
+    @Test
+    void ensureReadFileThrowsFileNotFoundException(){
+        //Act & Assert
+        assertThrows(FileNotFoundException.class, () -> {
+            File file = FileHandler.readFile("testfile.example");
+        });
+    }
+
+    @Test
+    void ensureReadFileThrowsInvalidArgumentException(){
+        //Arrange
+        String userDirectory = System.getProperty("user.dir");
+
+        //Act & Assert
+        assertThrows(IllegalArgumentException.class, () -> {
+            File file = FileHandler.readFile(userDirectory);
+        });
+    }
+
+
+    @Test
+    void ensureReadCSVWorks() throws InvalidFileTypeException, InvalidAttributeValueException {
+        //Arrange
+        //File testfile = generateTestFile();
+        //String testString = generateCSVString();
+        //List<?> testCSVList = CSVStringToList(testString);
 
         //Act
-        List<?> readData = FileHandler.readCSV(testfile);
+        //List<?> readData = FileHandler.readCSV(CSV_FILE_FILEPATH);
 
         //Assert
-        assertEquals(readData, testCSVList);
+        //assertEquals(readData, testCSVList);
 
+        //Arrange
+        File csvFile = new File(CSV_FILE_FILEPATH);
+
+
+
+        //List<?> data = FileHandler.readCSV(csvFile);
+
+        //Act & Assert
+        assertEquals(FileHandler.readCSV(csvFile), this.data);
+    }
+
+    @Test
+    void ensureAppendToFileWorks() throws IOException {
+        //Arrange
+        File f1 = new File(TEST_FILE_FILEPATH);
+        File f2 = new File(TEST_FILE_2_FILEPATH);
+
+        //Act
+        FileHandler.appendToFile(TEST_FILE_FILEPATH, "\n"+EXAMPLE_LINE_2);
+        //f1.equals(f2);
+
+
+        //Assert
+        //assertEquals(FileHandler.readFile(TEST_FILE_FILEPATH), FileHandler.readFile(TEST_FILE_2_FILEPATH));
+        assertEquals(-1L, Files.mismatch(f1.toPath(), f2.toPath()));
     }
 }
