c15b53f73854f4e93632223bcce41a4bbe198473
Tiago Correia <106349052+tiagohcorreia@users.noreply.github.com>
2023-05-23 10:48:33 +0100

    [Implementation] - PlaceOrderToBuyPropertyRepository.java updated

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/repository/PlaceOrderToBuyPropertyRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/repository/PlaceOrderToBuyPropertyRepository.java
@@ -1,4 +1,58 @@
 package pt.ipp.isep.dei.esoft.project.domain.repository;
 
+import pt.ipp.isep.dei.esoft.project.domain.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlaceOrderToBuyPropertyRepository {
+
+    public static List<Order> orderList = new ArrayList<>();
+
+    public boolean saveOrder(Order order) {
+
+        if(validateOrder(order)) {
+
+            return addOrder(order);
+        }
+        return false;
+    }
+
+    public boolean addOrder(Order order) {
+
+        if(order != null && validateOrder(order)) {
+
+            return this.orderList.add(order);
+        }
+        return false;
+    }
+
+    public boolean validateOrder(Order order) {
+
+        for(Order order1: orderList) {
+
+            if(order.equals(order)) {
+
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<Order> getOrderList() {
+
+        return new ArrayList<>(orderList);
+    }
+
+    public String getOrdersListAsString() {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(Order order: this.orderList) {
+
+            stringBuilder.append("-").append(order.toString()).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
 }
