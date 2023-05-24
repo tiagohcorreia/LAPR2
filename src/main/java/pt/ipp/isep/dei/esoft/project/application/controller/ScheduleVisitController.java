ed739b036ce08bc799230f17526a1e40faa1a4c7
Vitor Fardilha <1211273@isep.ipp.pt>
2023-05-24 23:22:13 +0100

    US09- Start implementing the Controller

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/ScheduleVisitController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/ScheduleVisitController.java
@@ -1,4 +1,15 @@
 package pt.ipp.isep.dei.esoft.project.application.controller;
 
+import pt.ipp.isep.dei.esoft.project.domain.repository.PlaceOrderToBuyPropertyRepository;
+import pt.ipp.isep.dei.esoft.project.domain.repository.ScheduleRepository;
+
 public class ScheduleVisitController {
+
+    private ScheduleRepository scheduleRepository;
+
+    public ScheduleVisitController(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+
+    }
+
 }
