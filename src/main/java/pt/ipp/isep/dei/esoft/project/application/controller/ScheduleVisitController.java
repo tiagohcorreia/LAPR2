abc88dbb7b54d5a25140791c6fbc379a63fda790
Vitor Fardilha <1211273@isep.ipp.pt>
2023-05-26 23:43:22 +0100

    US04- ScheduleVisitController update

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/ScheduleVisitController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/ScheduleVisitController.java
@@ -3,6 +3,8 @@ package pt.ipp.isep.dei.esoft.project.application.controller;
 import pt.ipp.isep.dei.esoft.project.domain.dto.AnnouncementDTO;
 import pt.ipp.isep.dei.esoft.project.domain.mappers.AnnouncementMapper;
 import pt.ipp.isep.dei.esoft.project.domain.model.Announcement;
+import pt.ipp.isep.dei.esoft.project.domain.model.Employee;
+import pt.ipp.isep.dei.esoft.project.domain.model.Role;
 import pt.ipp.isep.dei.esoft.project.domain.model.Schedule;
 import pt.ipp.isep.dei.esoft.project.domain.repository.AnnouncementRepository;
 import pt.ipp.isep.dei.esoft.project.domain.repository.ScheduleRepository;
@@ -10,6 +12,7 @@ import pt.ipp.isep.dei.esoft.project.domain.repository.ScheduleRepository;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ScheduleVisitController {
@@ -41,5 +44,29 @@ public class ScheduleVisitController {
         } catch (Exception e) {
             throw new IllegalArgumentException(e.getMessage().toString());
         }
+    }
+
+    public boolean validateScheduleHour(AnnouncementDTO announcementDTO, LocalDate day, LocalTime beginTime, LocalTime endTime) {
+        boolean isOverlap = false;
+        for(Schedule schedule : scheduleRepository.getScheduleList()) {
+
+            if (beginTime.isBefore(schedule.getEndHour()) && endTime.isAfter(schedule.getBeginHour())) {
+                isOverlap = true;
+                throw new IllegalArgumentException("That hour is already scheduled, please insert data again.");
+
+            } else if (beginTime.equals(schedule.getBeginHour()) || endTime.equals(schedule.getEndHour())) {
+                isOverlap = true;
+
+                throw new IllegalArgumentException("That hour is already scheduled, please insert data again.");
+            }
+
+            if (schedule.getAnnouncementDTO().equals(announcementDTO) && schedule.getDay().equals(day) && isOverlap==false) {
+                return true;
+            }
+        }
+        return true;
+    }
+    public AnnouncementDTO getAnnouncementDTO(int posAnnouncement) {
+        return AnnouncementMapper.getAnnouncementDTOById(posAnnouncement);
     }
 }
