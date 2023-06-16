1e20c282c11624bb99818e72d859a0700e21139b
Vitor Fardilha <1211273@isep.ipp.pt>
2023-06-16 21:17:30 +0100

    US019 - Filter By Houses And Apartments

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/AnalyseDealsController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/AnalyseDealsController.java
@@ -1,5 +1,24 @@
 package pt.ipp.isep.dei.esoft.project.application.controller;
 
-public class AnalyseDealsController {
+import pt.ipp.isep.dei.esoft.project.domain.model.Announcement;
+import pt.ipp.isep.dei.esoft.project.domain.model.Order;
+import pt.ipp.isep.dei.esoft.project.domain.repository.PurchaseOrderRepository;
+import pt.ipp.isep.dei.esoft.project.domain.repository.Repositories;
 
+import java.util.List;
+
+public class AnalyseDealsController {
+    Repositories repositories = Repositories.getInstance();
+    PurchaseOrderRepository purchaseOrderRepository= repositories.getPurchaseOrderRepository();
+
+    public List<Order> getApartmentsAndHouses(){
+        List<Order> orderList= purchaseOrderRepository.getAll();
+        List<Order> housesAndApartmentsList= purchaseOrderRepository.getAll();
+        for (Order order : orderList) {
+            if (order.getAnnouncementDTO().getProperty().getClass().toString().equals("House") || order.getAnnouncementDTO().getProperty().getClass().toString().equals("Apartment")) {
+                housesAndApartmentsList.add(order);
+            }
+        }
+        return housesAndApartmentsList;
+    }
 }
