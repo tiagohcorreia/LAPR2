c30a351d062e3c8554bcbb589bd073967a797a32
1210789 <1210789@isep.ipp.pt>
2023-06-18 23:19:39 +0100

    US17 - Implementation

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/ListDealsController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/ListDealsController.java
@@ -1,26 +1,47 @@
 package pt.ipp.isep.dei.esoft.project.application.controller;
 
-import javafx.collections.ObservableList;
+import pt.ipp.isep.dei.esoft.project.domain.dto.DealsDto;
 import pt.ipp.isep.dei.esoft.project.domain.model.Announcement;
 import pt.ipp.isep.dei.esoft.project.domain.repository.AnnouncementRepository;
+import pt.ipp.isep.dei.esoft.project.domain.repository.EmployeeRepository;
+import pt.ipp.isep.dei.esoft.project.domain.repository.PlaceOrderToBuyPropertyRepository;
 import pt.ipp.isep.dei.esoft.project.domain.repository.Repositories;
-import pt.ipp.isep.dei.esoft.project.domain.shared.AnnouncementDateComparator;
 import pt.ipp.isep.dei.esoft.project.domain.shared.SortingOrder;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class ListDealsController {
     static AnnouncementRepository announcementRepository = Repositories.getInstance().getAnnouncementRepository();
 
-
-    public static List<Announcement> getDeals() {
-        List<Announcement> announcements = new ArrayList<>();
-        announcements.addAll(announcementRepository.getSoldAnnouncements());
+//    public static List<Announcement> getDeals() {
+//        List<DealsDto> deals = new ArrayList<>();
+//        List<Announcement> announcements = new ArrayList<>();
+//        announcements.addAll(announcementRepository.getSoldAnnouncements());
+//        //announcements.sort(new AnnouncementDateComparator());
+//        AnnouncementRepository.sortAnnouncements(announcements, "date", SortingOrder.DESCENDING);
+//
+//        return announcements;
+//    }
+    public static List<DealsDto> getDeals() {
+        List<DealsDto> deals = new ArrayList<>();
+        List<Announcement> announcements = announcementRepository.getSoldAnnouncements();
+        for (Announcement a:
+             announcements) {
+            DealsDto newDto = new DealsDto(a);
+            deals.add(newDto);
+        }
         //announcements.sort(new AnnouncementDateComparator());
-        AnnouncementRepository.sortAnnouncements(announcements, "date", SortingOrder.DESCENDING);
-        return announcements;
+        //AnnouncementRepository.sortAnnouncements(announcements, "date", SortingOrder.DESCENDING);
+        return deals;
+    }
+
+    public static void sortDealsByDate(List<DealsDto> dealsDtoList, SortingOrder sortingOrder){
+        DealsDto.sortDealsByDate(dealsDtoList, sortingOrder);
+    }
+
+    public static void sortDealsWithAlgorithm(List<DealsDto> dealsDtoList, String algorithm, SortingOrder sortingOrder){
+
     }
 
 //    public static List<Announcement> sortByAreaWithAlgorithm(List<Announcement> announcements, String sortingAlgorithm, SortingOrder sortingorder){
